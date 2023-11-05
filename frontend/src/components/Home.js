90cda288689a6e40b881224aaa9c6d3a4732b614
Aashish Bhandari <jhurenas@gmail.com>
2023-11-05 08:28:43 +0545

    Modified shimmering loader

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -59,122 +59,124 @@ const Home = () => {
   }, [pathname]);
   return (
     <Fragment>
-      {isFetching ? (
-        <HomeLoader />
-      ) : (
-        <div className="container container-fluid">
-          <MetaData title={"Buy Best Products Online"} />
+      <div className="container container-fluid">
+        <MetaData title={"Buy Best Products Online"} />
 
-          <h1 id="products_heading">Latest Products</h1>
+        <h1 id="products_heading">Latest Products</h1>
 
-          <section id="products" className="container mt-5">
-            <div className="row">
-              {keyword ? (
-                <Fragment>
-                  <div className="col-6 col-md-3 mt-5 mb-5">
-                    <div className="px-5">
-                      <Range
-                        marks={{
-                          1: `$1`,
-                          1000: `$1000`,
-                        }}
-                        min={1} //minimum value of the slider
-                        max={1000} //maximum value of the slider
-                        // defaultValue={[2, 100]}//We dont need it
-                        tipFormatter={(value) => `$${value}`} //function format tooltip's overlay
-                        tipProps={{
-                          placement: "top",
-                          visible: true,
-                        }}
-                        value={price} //Sets current value of slider
-                        onChange={(price) => setPrice(price)}
-                      />
+        <section id="products" className="container mt-5">
+          <div className="row">
+            {keyword ? (
+              <Fragment>
+                <div className="col-6 col-md-3 mt-5 mb-5">
+                  <div className="px-5">
+                    <Range
+                      marks={{
+                        1: `$1`,
+                        1000: `$1000`,
+                      }}
+                      min={1} //minimum value of the slider
+                      max={1000} //maximum value of the slider
+                      // defaultValue={[2, 100]}//We dont need it
+                      tipFormatter={(value) => `$${value}`} //function format tooltip's overlay
+                      tipProps={{
+                        placement: "top",
+                        visible: true,
+                      }}
+                      value={price} //Sets current value of slider
+                      onChange={(price) => setPrice(price)}
+                    />
 
-                      <hr className="my-5" />
+                    <hr className="my-5" />
 
-                      <div className="mt-5">
-                        <h4 className="mb-3">Categories</h4>
+                    <div className="mt-5">
+                      <h4 className="mb-3">Categories</h4>
 
-                        <ul className="pl-0">
-                          {categories?.map((category) => (
-                            <li
-                              style={{
-                                cursor: "pointer",
-                                listStyleType: "none",
-                              }}
-                              key={category}
-                              onClick={() => setCategory(category)}
-                            >
-                              {category}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
+                      <ul className="pl-0">
+                        {categories?.map((category) => (
+                          <li
+                            style={{
+                              cursor: "pointer",
+                              listStyleType: "none",
+                            }}
+                            key={category}
+                            onClick={() => setCategory(category)}
+                          >
+                            {category}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
 
-                      <hr className="my-3" />
+                    <hr className="my-3" />
 
-                      <div className="mt-5">
-                        <h4 className="mb-3">Ratings</h4>
+                    <div className="mt-5">
+                      <h4 className="mb-3">Ratings</h4>
 
-                        <ul className="pl-0">
-                          {[5, 4, 3, 2, 1].map((star) => (
-                            <li
-                              style={{
-                                cursor: "pointer",
-                                listStyleType: "none",
-                              }}
-                              key={star}
-                              onClick={() => setRating(star)}
-                            >
-                              <div className="rating-outer">
-                                <div
-                                  className="rating-inner"
-                                  style={{
-                                    width: `${star * 20}%`,
-                                  }}
-                                ></div>
-                              </div>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
+                      <ul className="pl-0">
+                        {[5, 4, 3, 2, 1].map((star) => (
+                          <li
+                            style={{
+                              cursor: "pointer",
+                              listStyleType: "none",
+                            }}
+                            key={star}
+                            onClick={() => setRating(star)}
+                          >
+                            <div className="rating-outer">
+                              <div
+                                className="rating-inner"
+                                style={{
+                                  width: `${star * 20}%`,
+                                }}
+                              ></div>
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
                     </div>
                   </div>
+                </div>
 
-                  <div className="col-6 col-md-9">
-                    <div className="row">
-                      {data?.products.map((product) => (
+                <div className="col-6 col-md-9">
+                  <div className="row">
+                    {isFetching ? (
+                      <HomeLoader col={4} />
+                    ) : (
+                      data?.products.map((product) => (
                         <Product key={product._id} product={product} col={4} />
-                      ))}
-                    </div>
+                      ))
+                    )}
                   </div>
-                </Fragment>
-              ) : (
-                data?.products.map((product) => (
-                  <Product key={product._id} product={product} col={3} />
-                ))
-              )}
-            </div>
-          </section>
+                </div>
+              </Fragment>
+            ) : isFetching ? (
+              <HomeLoader col={3} />
+            ) : (
+              data?.products.map((product) => (
+                <Product key={product._id} product={product} col={3} />
+              ))
+            )}
+          </div>
+        </section>
 
-          {data?.resPerPage <= count && (
-            <div className="d-flex justify-content-center mt-5">
-              <Pagination
-                activePage={currentPage}
-                itemsCountPerPage={data?.resPerPage} //8
-                totalItemsCount={data?.productsCount} //9
-                onChange={setCurrentPageNo}
-                nextPageText={"Next"}
-                prevPageText={"Prev"}
-                firstPageText={"First"}
-                lastPageText={"Last"}
-                itemClass="page-item"
-                linkClass="page-link"
-              />
-            </div>
-          )}
-        </div>
-      )}
+        {data?.resPerPage <= count && (
+          <div className="d-flex justify-content-center mt-5">
+            <Pagination
+              activePage={currentPage}
+              itemsCountPerPage={data?.resPerPage} //8
+              totalItemsCount={data?.productsCount} //9
+              onChange={setCurrentPageNo}
+              nextPageText={"Next"}
+              prevPageText={"Prev"}
+              firstPageText={"First"}
+              lastPageText={"Last"}
+              itemClass="page-item"
+              linkClass="page-link"
+            />
+          </div>
+        )}
+      </div>
     </Fragment>
   );
 };
